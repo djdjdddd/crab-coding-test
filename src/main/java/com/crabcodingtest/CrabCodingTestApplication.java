db82de8a84be4255dd3db48eafc2256b08a8ec70
djdjdddd <djdj2297@gmail.com>
2024-01-16 22:03:31 +0900

    Feat: crab-coding 테스트 코드 전용 프로젝트 생성 완료

---
+++ src/main/java/com/crabcodingtest/CrabCodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.crabcodingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrabCodingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrabCodingTestApplication.class, args);
+    }
+
+}
