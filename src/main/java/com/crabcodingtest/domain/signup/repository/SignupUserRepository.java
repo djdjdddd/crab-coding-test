7613238039367c29b00a2d13e134ab4209bde675
djdjdddd <djdj2297@gmail.com>
2024-01-19 01:15:36 +0900

    Feat: 회원가입 Repository

---
+++ src/main/java/com/crabcodingtest/domain/signup/repository/SignupUserRepository.java
@@ -0,0 +1,9 @@
+package com.crabcodingtest.domain.signup.repository;
+
+import com.crabcodingtest.domain.signup.entity.SignupUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SignupUserRepository extends JpaRepository<SignupUser, Long> { // CRUD랑 JPA 차이가 뭐더라??
+
+}
