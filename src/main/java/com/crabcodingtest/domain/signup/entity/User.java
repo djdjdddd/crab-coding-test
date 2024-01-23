536e662f10171725c50c1d3e01b8331cbc29f745
djdjdddd <djdj2297@gmail.com>
2024-01-23 23:40:38 +0900

    Feat: 회원가입 테스트용 User Entity 생성

---
+++ src/main/java/com/crabcodingtest/domain/signup/entity/User.java
@@ -0,0 +1,37 @@
+package com.crabcodingtest.domain.signup.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Getter @Setter
+@Builder // @AllArgsConstructor를 필요로 함.
+@ToString
+//@NoArgsConstructor // 에러남
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class User {
+    // 이름
+    // 이메일
+    // 비밀번호
+    // 비밀번호 재입력 (이건 프론트단에서 끝내자)
+    // 닉네임
+    // 핸드폰 번호
+    // 주소 (API 쓰기로 했었으니까)
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String mail;
+
+    private String nickname;
+
+    private String phoneNo;
+    @Embedded
+    private Address address;
+}
