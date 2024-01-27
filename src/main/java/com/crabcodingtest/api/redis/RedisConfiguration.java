49db3f3b6fb5c94278299526d74918726909f8fe
djdjdddd <djdj2297@gmail.com>
2024-01-28 02:11:31 +0900

    Feat: Configuration of Embedded Redis Server

---
+++ src/main/java/com/crabcodingtest/api/redis/RedisConfiguration.java
@@ -0,0 +1,30 @@
+package com.crabcodingtest.api.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+/**
+ * Embedded Redis Server용 Configuration
+ */
+@Configuration
+@EnableRedisRepositories // Annotation to activate Redis repositories. (공식문서 참조 : https://docs.spring.io/spring-data/redis/docs/current-SNAPSHOT/api/org/springframework/data/redis/repository/configuration/EnableRedisRepositories.html)
+public class RedisConfiguration {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(RedisProperties redisProperties){
+        return new LettuceConnectionFactory(
+                redisProperties.getRedisHost(),
+                redisProperties.getRedisPort()
+        );
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(LettuceConnectionFactory connectionFactory){
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
