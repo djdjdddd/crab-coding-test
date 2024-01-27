498aef63396ef09d67341f14e512b60c165b225c
djdjdddd <djdj2297@gmail.com>
2024-01-28 02:11:41 +0900

    Feat: Properties of Embedded Redis Server

---
+++ src/main/java/com/crabcodingtest/api/redis/RedisProperties.java
@@ -0,0 +1,23 @@
+package com.crabcodingtest.api.redis;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Embedded Redis Server용 Properties
+ */
+@Getter
+@Configuration
+public class RedisProperties {
+    private int redisPort;
+    private String redisHost;
+
+    public RedisProperties(
+            @Value("${spring.data.redis.port}") int redisPort, // @Value 고도화 (by Tecoble ??)
+            @Value("${spring.data.redis.host}") String redisHost){
+        this.redisPort = redisPort;
+        this.redisHost = redisHost;
+    }
+}
