6a7d43675ab49ba4f8bcdeefaa0ae794338238c4
djdjdddd <djdj2297@gmail.com>
2024-01-29 23:49:40 +0900

    Feat: 정적 팩토리 메서드 of(authCode, mail)

--- src/main/java/com/crabcodingtest/api/redis/Authentication.java
+++ src/main/java/com/crabcodingtest/api/redis/Authentication.java
@@ -7,7 +7,6 @@ import lombok.ToString;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
-@Setter
 @Getter
 @ToString
 // https://mingyum119.tistory.com/266
@@ -16,6 +15,17 @@ public class Authentication {
     @Id // Redis 해시의 키로 사용되어, 객체를 고유하게 식별되는 것에 사용된다.
     private String id; // Spring Data Redis를 사용할 경우 @Id는 반드시 이것만 가능한 듯. (id 말고 mail을 @Id로 쓰려니까 에러 발생 : Entity com.coding.crab.api.redis.TestUser requires to have an explicit id field; Did you forget to provide one using @Id)
 
-    @Indexed // Secondary Indexes의 역할을 하도록 하는 역할. 필요 시 이 값으로 데이터를 조회하기 위해 인덱싱을 해주는 것이다.
+    //@Indexed // Secondary Indexes의 역할을 하도록 하는 역할. 필요 시 이 값으로 데이터를 조회하기 위해 인덱싱을 해주는 것이다.
     private String authCode;
+
+    private String mail;
+
+    // 정적 팩토리 메서드 : 객체 불변성을 지키고자 Setter 사용 X
+    private Authentication(String authCode, String mail){
+        this.authCode = authCode;
+        this.mail = mail;
+    }
+    public static Authentication of(String authCode, String mail){
+        return new Authentication(authCode, mail);
+    }
 }
