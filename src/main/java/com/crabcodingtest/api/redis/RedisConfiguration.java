6f5915cc9cd60727343907922e46396588786edd
djdjdddd <djdj2297@gmail.com>
2024-01-29 22:40:24 +0900

    Feat: Redis의 keyspace notifications 기능 활성화
    - @EnableRedisRepositories 어노테이션에 enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP 추가하여 활성화
    - Spring Data Redis는 이 Key Space Notifications 기능을 사용하여 TTL 만료 이벤트를 감지하고, 이를 통해 @Indexed로 지정된 보조 인덱스를 함께 삭제

--- src/main/java/com/crabcodingtest/api/redis/RedisConfiguration.java
+++ src/main/java/com/crabcodingtest/api/redis/RedisConfiguration.java
@@ -3,6 +3,7 @@ package com.crabcodingtest.api.redis;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
@@ -10,7 +11,8 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
  * Embedded Redis Server용 Configuration
  */
 @Configuration
-@EnableRedisRepositories // Annotation to activate Redis repositories. (공식문서 참조 : https://docs.spring.io/spring-data/redis/docs/current-SNAPSHOT/api/org/springframework/data/redis/repository/configuration/EnableRedisRepositories.html)
+// Annotation to activate Redis repositories. (공식문서 참조 : https://docs.spring.io/spring-data/redis/docs/current-SNAPSHOT/api/org/springframework/data/redis/repository/configuration/EnableRedisRepositories.html)
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfiguration {
 
     @Bean
