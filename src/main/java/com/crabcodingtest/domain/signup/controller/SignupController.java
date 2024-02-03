d88679795eb235b1bfafb561f4760d4a65ab93d9
djdjdddd <djdj2297@gmail.com>
2024-02-03 22:35:22 +0900

    Docs: Swagger 어노테이션 테스트 중

--- src/main/java/com/crabcodingtest/domain/signup/controller/SignupController.java
+++ src/main/java/com/crabcodingtest/domain/signup/controller/SignupController.java
@@ -1,15 +1,15 @@
 package com.crabcodingtest.domain.signup.controller;
 
 import com.crabcodingtest.api.mail.MailDTO;
+import com.crabcodingtest.api.mail.MailSender;
 import com.crabcodingtest.api.mail.MailService;
 import com.crabcodingtest.domain.signup.service.SignupService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "회원가입", description = "회원가입 API") // swagger 적용 위한 어노테이션
 @Slf4j
@@ -21,14 +21,15 @@ public class SignupController {
     private final SignupService signupService;
 
     @PostMapping("/mail-test")
-    public void mailTest(){
+    @Operation(summary = "메일 인증", description = "회원가입시 필요한 메일 인증을 요청합니다. 요청시 인증 코드가 발송됩니다.")
+    public void mailTest(@Parameter MailDTO mailDTO){
         log.info("mail-test 요청 들어옴");
 
-        MailDTO mailDTO = new MailDTO();
-        mailDTO.setUserName("김용희");
-        mailDTO.setUserMail("djdjdddd@khu.ac.kr");
+        MailDTO testDTO = new MailDTO();
+        testDTO.setUserName("김용희");
+        testDTO.setUserMail("djdjdddd@khu.ac.kr");
 
-        signupService.sendAuthCodeMail(mailDTO);
+        signupService.sendAuthCodeMail(testDTO);
     }
 
 }
