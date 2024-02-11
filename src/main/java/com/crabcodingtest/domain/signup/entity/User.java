1732adf9bce52edc00d6658a0842df96f2790ea9
djdjdddd <djdj2297@gmail.com>
2024-02-12 01:48:09 +0900

    Feat: UserType, AuthorityType 필드 추가

--- src/main/java/com/crabcodingtest/domain/signup/entity/User.java
+++ src/main/java/com/crabcodingtest/domain/signup/entity/User.java
@@ -1,29 +1,65 @@
 package com.crabcodingtest.domain.signup.entity;
 
+import com.crabcodingtest.common.AuthorityType;
+import com.crabcodingtest.common.BaseEntity;
+import com.crabcodingtest.common.UserType;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Entity
-@Table(name = "user")
-@Getter @Setter
-@Builder
-@ToString
-@AllArgsConstructor
-@RequiredArgsConstructor
-public class User {
-    // 이름
-    // 이메일
-    // 비밀번호
-    // 비밀번호 재입력 (이건 프론트단에서 끝내자)
-    // 닉네임
-    // 핸드폰 번호
-    // 주소 (API 쓰기로 했었으니까)
-    @Id @GeneratedValue
-    @Column(name = "user_id")
-    private Long id;
-    private String name;
-    private String password;
+@Table(name = "TBL_USER")
+@Getter
+@Setter // <------------ 나중에 지워야 함. (테스트 코드용)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Column(nullable = false, length = 20)
+    private String name;        // 이름
+
+    @Column(unique = true)
     private String mail;
-    private String nickname;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;    // 닉네임
+
+    private String password;
+
     private String phoneNo;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private UserType type;
+
+    // 회원은 기본적으로 ROLE_USER 권한을 가지지만 추가적으로 권한을 더 가질 수 있기 때문에 List 타입으로 선언했습니다.
+    // 그리고 권한 정보의 경우 독자적으로 조회되는 경우는 없기 때문에 별도의 엔티티로 구현하는 대신 ElementCollection 으로 처리하였습니다.
+    @ElementCollection(targetClass = AuthorityType.class)
+    @CollectionTable(name = "TBL_USER_AUTHORITY", joinColumns = @JoinColumn(name = "USER_ID"))
+    @Enumerated(EnumType.STRING)
+    private List<AuthorityType> authorities = new ArrayList<>();
+
+    @Builder
+    public User(String name, String password, String mail, String nickname, String phoneNo, String address, UserType type){
+        this.name = name;
+        this.password = password;
+        this.mail = mail;
+        this.nickname = nickname;
+        this.phoneNo = phoneNo;
+        this.address = address;
+        this.authorities.add(AuthorityType.ROLE_USER); // 회원은 기본적으로 ROLE_USER 권한을 가지지만 추가적으로 권한을 더 가질 수 있기 때문에 List 타입으로 선언했습니다.... (중략)
+        this.type = type;
+    }
+
+    // ** 스프링 시큐리티 의존성 추가하면 주석 해제할 부분. (import org.springframework.security.core.GrantedAuthority;)
+    /*
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return this.authorities.stream().map(authority -> new SimpleGrantedAuthority(authority.toString())).collect(Collectors.toList());
+    }
+    */
 }
+
