fc816e8f9ea5a320bf74798d104deb38d5236697
djdjdddd <djdj2297@gmail.com>
2024-02-12 01:52:25 +0900

    Docs: 필드별 Validation 적용

--- src/main/java/com/crabcodingtest/domain/signup/request/SignupRequestDTO.java
+++ src/main/java/com/crabcodingtest/domain/signup/request/SignupRequestDTO.java
@@ -3,25 +3,35 @@ package com.crabcodingtest.domain.signup.request;
 import com.crabcodingtest.domain.signup.entity.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
-@ToString
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SignupRequestDTO {
+
     @NotBlank
+    @Size(max = 20, message = "너무 긴 이름입니다. (최대 20자)")
     private String name;                // 이름
+
     @NotBlank
-    @Email
+    @Email(message = "이메일 형식이 잘못되었습니다.")
     private String mail;                // 이메일
+
     //@NotBlank
     private String nickname;            // 닉네임
-    //@NotBlank
+
+    // TODO. 개발 완료되면 {1,20} ---> {8,20} 으로 바꾸기
+    @Pattern(regexp = "[a-zA-Z!@#$%^&*-_]{1,20}", message = "1~20 길이의 알파벳과 숫자, 특수문자만 사용할 수 있습니다.")
     private String password;            // 비밀번호
+
     //@NotBlank
     private String phoneNo;             // 핸드폰 번호
+
     //@NotBlank
     private String address;             // 주소
+
     private String authenticationCode;  // 메일 인증코드
 
     @Builder
@@ -34,6 +44,7 @@ public class SignupRequestDTO {
         this.address = address;
     }
 
+    // TODO. 리팩토링 필요
     public User toEntity(){
         return User.builder()
                 .name(name)
